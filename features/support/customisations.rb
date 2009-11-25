f915a74b04b52080f2eef2a1b0d84ff2e0088812
Andy Shen <andy@shenie.info>
2009-11-25 11:04:44 +1100

    extract customizations from env.rb for easier future cucumber upgrades

---
+++ features/support/customisations.rb
@@ -0,0 +1,11 @@
+
+fixtures_folder = File.join(RAILS_ROOT, 'spec', 'fixtures')
+Fixtures.create_fixtures(fixtures_folder, %w[plans site_settings])
+
+require 'pickle/world'
+# Example of configuring pickle:
+#
+# Pickle.configure do |config|
+#   config.adapters = [:machinist]
+#   config.map 'I', 'myself', 'me', 'my', :to => 'user: "me"'
+# end
--- features/support/env.rb
+++ features/support/env.rb
@@ -45,13 +45,3 @@ Webrat.configure do |config|
   config.open_error_files = false # Set to true if you want error pages to pop up in the browser
 end
 
-fixtures_folder = File.join(RAILS_ROOT, 'spec', 'fixtures')
-Fixtures.create_fixtures(fixtures_folder, %w[site_settings])
-
-require 'pickle/world'
-# Example of configuring pickle:
-#
-# Pickle.configure do |config|
-#   config.adapters = [:machinist]
-#   config.map 'I', 'myself', 'me', 'my', :to => 'user: "me"'
-# end
